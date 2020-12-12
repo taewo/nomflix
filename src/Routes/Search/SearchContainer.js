0f7beec18315908fd8f67293f002642242a25149
zorba <zorba@sentbe.com>
2020-12-13 01:23:09 +0900

    Connect API SearchContainer

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { moviesApi, tvApi } from '../../api'
 import SearchPresenter from './SearchPresenter'
 
 export default class extends React.Component {
@@ -10,6 +11,36 @@ export default class extends React.Component {
     loading: false
   }
 
+  handleSubmit = () => {
+    const { searchTerm } = this.state
+    if (searchTerm !== '') {
+      this.searchByTerm()
+    }
+  }
+
+  searchByTerm = async () => {
+    const { searchTerm } = this.state
+    try {
+      this.setState({
+        loading: true
+      })
+      const { data: { results: movieResults }} = await moviesApi.search(searchTerm)
+      const { data: { results: tvResults }} = await tvApi.search(searchTerm)
+      this.setState({
+        movieResults,
+        tvResults
+      })
+    } catch {
+      this.setState({
+        error: 'Can"t find result.'
+      })
+    } finally {
+      this.setState({
+        loading: false
+      })
+    }
+  }
+
   render() {
     const { movieResults, tvResults, searchTerm, error, loading } = this.state
     return (
@@ -19,6 +50,7 @@ export default class extends React.Component {
         searchTerm={searchTerm}
         error={error}
         loading={loading}
+        handleSubmit={this.handleSubmit}
       />
     )
   }
