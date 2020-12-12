063a9fa2657c248acf87c74c2be6e111a2ce93a6
zorba <zorba@sentbe.com>
2020-12-13 01:06:20 +0900

    Update API code

--- src/api.js
+++ src/api.js
@@ -9,37 +9,37 @@ const api = axios.create({
 })
 
 export const moviesApi = {
-  nowPlaying: () => api.get('movie/now_playing'),
-  upcoming: () => api.get('movie/upcoming'),
-  popular: () => api.get('movie/popular'),
+  nowPlaying: () => api.get("movie/now_playing"),
+  upcoming: () => api.get("movie/upcoming"),
+  popular: () => api.get("movie/popular"),
   movieDetail: id =>
     api.get(`movie/${id}`, {
       params: {
-        append_to_response: 'videos'
+        append_to_response: "videos"
       }
     }),
   search: term =>
-    api.get('search/movie', {
+    api.get("search/movie", {
       params: {
         query: encodeURIComponent(term)
       }
     })
-}
+};
 
 export const tvApi = {
-  topRated: () => api.get('movie/top_rated'),
-  popular: () => api.get('movie/popular'),
-  airingToday: () => api.get('movie/airing_today'),
+  topRated: () => api.get("tv/top_rated"),
+  popular: () => api.get("tv/popular"),
+  airingToday: () => api.get("tv/airing_today"),
   showDetail: id =>
     api.get(`tv/${id}`, {
       params: {
-        append_to_response: 'videos'
+        append_to_response: "videos"
       }
     }),
   search: term =>
-    api.get('search/tv', {
+    api.get("search/tv", {
       params: {
         query: encodeURIComponent(term)
       }
     })
-}
+};
